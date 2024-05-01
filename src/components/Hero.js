508de6d656d119cf6b47538b9c1fd13c839d365e
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2024-05-01 12:04:35 -0300

    add(Hero): Add hero content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,11 @@
+import Button from "@/components/Button";
+
 const Hero = () => {
   return (
-    <section className="relative h-screen flex flex-col items-center justify-center text-center text-white py-0 px-3">
-      <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+    <section className="flex flex-col items-center justify-center text-white py-0 px-0 bg-primary">
+      <div className="video-docker top-0 left-0 overflow-hidden]">
         <video
-          className="min-w-full absolute object-cover"
+          className="min-w-full object-cover"
           src="https://cdn.shopify.com/videos/c/o/v/3e806d3996d243628daccc983b0f50a1.mp4"
           type="video/mp4"
           autoPlay
@@ -11,6 +13,19 @@ const Hero = () => {
           loop
           playsInline
         ></video>
+      </div>
+      <div className="my-12 text-center">
+        <h3 className="text-secondary tracking-widest ">DESDE 2016</h3>
+        <h2 className="py-2 text-white">Te damos la bienvenida a Cavaliere</h2>
+        <p className="text-secondary tracking-wide">- - - - -</p>
+        <p className=" py-2 text-white max-w-xl">
+          Fundada en el a;o 2016, Cavaliere es una Vinoteca Boutique atendida
+          por su propio due;o. Podras encontrar vinos de autor, bebidas
+          nacionales e internacionales. Regalos, cervezas y mucho mas...
+        </p>
+      </div>
+      <div>
+        <Button>Nuestra historia</Button>
       </div>
     </section>
   );
