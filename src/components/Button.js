e309ba85a27f3d585b89ed794de4929699c3c44a
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2025-03-26 11:41:35 -0300

    update versions

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 const Button = ({ children }) => {
   return (
-    <button className="text-secondary border-2 py-2 px-4 border-secondary uppercase tracking-widest hover:bg-secondary hover:text-white">
+    <button className="text-secondary rounded-full border-2 py-2 px-4 border-secondary uppercase tracking-widest hover:bg-secondary hover:text-white">
       {children}
     </button>
   );
