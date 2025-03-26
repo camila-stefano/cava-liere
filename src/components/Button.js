35207d8482f1a0079ac228e33f069f880f4fa27b
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2025-03-26 13:14:22 -0300

    add prettier plugin taildwind

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 const Button = ({ children }) => {
   return (
-    <button className="text-secondary rounded-full border-2 py-2 px-4 border-secondary uppercase tracking-widest hover:bg-secondary hover:text-white">
+    <button className="text-secondary border-secondary hover:bg-secondary rounded-full border-2 px-4 py-2 tracking-widest uppercase hover:text-white">
       {children}
     </button>
   );
