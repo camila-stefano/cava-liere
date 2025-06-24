894b848f66ba5aa5b79dffc404dced1db9d2d195
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2025-06-24 16:08:45 -0300

    fix bordr hover

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const NavLink = ({ href, label }) => {
   return (
-    <Link href={href} className="border-b-2 px-4 py-2 text-sm font-medium">
+    <Link href={href} className="text-sm font-medium hover:border-b-2">
       {label}
     </Link>
   );
